488bfdfbeded6a5a2cd1c0cefed767a5fdea3bac
TimPietrusky <timpietrusky@gmail.com>
2023-04-07 12:55:21 +0200

    feat: mobile support and utils to resize the canvas on window resize

---
+++ project/public/utils.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("canvas");
+
+window.addEventListener("resize", () => {
+	this.canvas.width = window.innerWidth;
+	this.canvas.height = window.innerHeight;
+});
